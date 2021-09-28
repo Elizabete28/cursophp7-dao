b3f43dbbca87397470364849af7f0a87d4c99c4b
ElizaSouza <eliza_cristina20@outlook.com>
2021-09-28 13:21:50 -0300

    Metodos de Listas

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+/*$root = new Usuario();
 
 $root->loadbyId(3);
 
-echo $root;
+echo $root;*/
+
+// Carregando uma lista de usuários
+//$lista = Usuario ::getList();
+
+// Carregando uma lista de usuários buscando pelo login
+
+//$search= Usuario ::search("d");
+//echo json_encode($search);
+
+//Carregando um usuário usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("Daniel","23456");
+
+echo $usuario;
 
 ?>
